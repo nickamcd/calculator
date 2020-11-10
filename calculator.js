e363b7c49687a5252fcc108bba77cc566591ca0b
Nick McDaniel <nickamcd@gmail.com>
2020-11-10 15:19:34 -0800

    Test commit.

--- calculator.js
+++ calculator.js
@@ -4,19 +4,13 @@ const subtract = (a, b) => a - b;
 
 const multiply = (a, b) => a * b;
 
-const divide = (a, b) => {
+const divide = function(a, b) {
     if (b == 0) return undefined;
     return a / b;
 }
 
 const operate = (operator, a ,b) => operator(a, b);
 
-const populateDisplay = input => {
-    alert(input);
-}
-
-let buttons = document.getElementsByClassName("num");
-
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", console.log("test"));
+const populateDisplay = function() {
+    
 }
