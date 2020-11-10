acb7edc8a98074930bafa9360434b6b0b569b3da
Nick McDaniel <nickamcd@gmail.com>
2020-11-10 13:30:47 -0800

    Added event listeners to buttons.

--- calculator.js
+++ calculator.js
@@ -4,9 +4,18 @@ const subtract = (a, b) => a - b;
 
 const multiply = (a, b) => a * b;
 
-const divide = function(a, b) {
+const divide = (a, b) => {
     if (b == 0) return undefined;
     return a / b;
 }
 
 const operate = (operator, a ,b) => operator(a, b);
+
+const populateDisplay = input => {
+    console.log(input);
+}
+
+let buttons = document.getElementsByClassName("num");
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", populateDisplay(buttons[i].textContent));
+}
