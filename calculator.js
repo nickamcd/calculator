821538acefb46a5410adc14b645d4f2ab80eabb8
Nick McDaniel <nickamcd@gmail.com>
2020-11-13 01:22:28 -0800

    Fiddling with js.:

--- calculator.js
+++ calculator.js
@@ -4,7 +4,7 @@ const subtract = (a, b) => a - b;
 
 const multiply = (a, b) => a * b;
 
-const divide = function(a, b) {
+const divide = (a, b) => {
     if (b == 0) return undefined;
     return a / b;
 }
@@ -13,6 +13,8 @@ const operate = (operator, a ,b) => operator(a, b);
 
 const populateDisplay = (input) => {
     console.log(input);
+    let displayBoard = document.getElementById("display");
+    displayBoard.append(input);
 }
 
 let buttons = document.querySelectorAll(".num");
