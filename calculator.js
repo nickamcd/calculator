995a14d0b8d17277632a0aad99a0d469cba36465
Nick McDaniel <nickamcd@gmail.com>
2020-11-19 13:34:51 -0800

    Added clear function and fixed display.

--- calculator.js
+++ calculator.js
@@ -1,3 +1,9 @@
+// variables for storing values to evaluate
+let value1;
+let value2;
+let operand;
+
+// Basic calculator functions
 const add = (a, b) => a + b;
 
 const subtract = (a, b) => a - b;
@@ -12,19 +18,17 @@ const divide = (a, b) => {
 const operate = (operator, a ,b) => operator(a, b);
 
 const populateDisplay = input => {
-    console.log(input);
     let displayBoard = document.getElementById("display");
-    displayBoard.append(input);
+    displayBoard.textContent += input;
+    console.log(displayBoard.textContent);
 }
 
-const evaluate = () => {
-    console.log("evaluating...");
-};
-
 const clear = () => {
-    document.getElementById("display").value = "";
+    document.getElementById("display").innerHTML = "";
 };
 
+
+// Add event listeners to each button
 let numButtons = document.querySelectorAll(".num");
 
 numButtons.forEach(button => {
@@ -37,12 +41,24 @@ let opButtons = document.querySelectorAll(".op");
 
 opButtons.forEach(button => {
     button.addEventListener("click", (e) => {
+        document.querySelector("display").textContent;
         populateDisplay(button.textContent);
+        value1 = document.querySelector("display").textContent;
+        operand = button.textContent
+        console.log(value1);
+        console.log(operand);
+        clear();
     });
 });
 
 let evalButton = document.querySelector(".eval");
 
 evalButton.addEventListener("click", (e) => {
-    evaluate();
+    console.log("evaluating...");
+    value2 = document.querySelector("display").textContent;
+    console.log(operate(operand, value1, value2));
 });
+
+let clearButton = document.querySelector(".clear");
+
+clearButton.addEventListener("click", (e) => clear());
